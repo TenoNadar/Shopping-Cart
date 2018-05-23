da7cad9847840e235c8657254d5eec0c98eb2be3
TitoNadar <titonadar06@gmail.com>
2018-05-23 23:24:47 +0530

    Body Parser

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
 const productRoutes = require('./API/routes/products');
 const orderRoutes = require('./API/routes/order');
 
 app.use(morgan('dev'));
-
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes );
 
