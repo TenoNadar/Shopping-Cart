ce5a54c0b6139c2f0a284fcf52fd03a33254ba7b
TitoNadar <titonadar06@gmail.com>
2018-05-23 22:48:32 +0530

    morgan installed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-
+const morgan = require('morgan')
 const productRoutes = require('./API/routes/products');
 const orderRoutes = require('./API/routes/order');
 
-
+app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes );
 
