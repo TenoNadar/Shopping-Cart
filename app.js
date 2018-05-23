077d6c74e3f37b320af2bf6a8af3524b8a9fef58
TitoNadar <titonadar06@gmail.com>
2018-05-23 23:13:38 +0530

    Handling error

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ const productRoutes = require('./API/routes/products');
 const orderRoutes = require('./API/routes/order');
 
 app.use(morgan('dev'));
+
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes );
 
+
+app.use((req , res , next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error); 
+})
+
+app.use((error,req , res , next) => {
+    
+    res.status(error.status || 500);
+    res.json({
+        error:{
+           message:error.message 
+        }
+    });
+     
+});
 
  module.exports = app;
